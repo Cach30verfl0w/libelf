26766d7d8a73997b26c3ff9c3b632b653d4a736d
Cach30verfl0w <cach30verfl0w@gmail.com>
2023-10-14 18:05:12 +0200

    Add documentation for ProgramHeader

--- src/header/mod.rs
+++ src/header/mod.rs
@@ -293,19 +293,73 @@ impl From<u32> for SegmentType {
     }
 }
 
+/// This structure contains the program segment header in a ELF file. The header contains the type,
+/// the flags, offset, virtual and physical address, file and memory size and alignment of the
+/// program section.
+///
+/// ## See also
+/// - [Program Header](https://www.sco.com/developers/gabi/latest/ch5.pheader.html) by SCO, Inc.
 #[derive(Clone, Copy, Ord, PartialOrd, Eq, PartialEq, Debug, Hash, Default)]
 pub struct ProgramHeader {
+    /// This field represents the type of the segment. For more information, see [SegmentType].
+    ///
+    /// ## See also
+    /// - [Program Header](https://www.sco.com/developers/gabi/latest/ch5.pheader.html) by SCO, Inc.
     pub ty: SegmentType,
+
+    /// This field represents the flags of the segment.
+    ///
+    /// ## See also
+    /// - [Program Header](https://www.sco.com/developers/gabi/latest/ch5.pheader.html) by SCO, Inc.
     pub flags: u32,
+
+    /// This field indicates the offset of the segment in the ELF data.
+    ///
+    /// ## See also
+    /// - [Program Header](https://www.sco.com/developers/gabi/latest/ch5.pheader.html) by SCO, Inc.
     pub offset: u64,
+
+    /// This field indicates the virtual address of the first byte in the memory.
+    ///
+    /// ## See also
+    /// - [Program Header](https://www.sco.com/developers/gabi/latest/ch5.pheader.html) by SCO, Inc.
     pub virtual_address: u64,
+
+    /// On systems for which physical addressing is relevant, this member is reserved for the
+    /// segment's physical address.
+    ///
+    /// ## See also
+    /// - [Program Header](https://www.sco.com/developers/gabi/latest/ch5.pheader.html) by SCO, Inc.
     pub physical_address: u64,
+
+    /// This field indicates the size of the segment in the ELF file.
+    ///
+    /// ## See also
+    /// - [Program Header](https://www.sco.com/developers/gabi/latest/ch5.pheader.html) by SCO, Inc.
     pub file_size: u64,
+
+    /// This field indicates the size of the segment in the memory.
+    ///
+    /// ## See also
+    /// - [Program Header](https://www.sco.com/developers/gabi/latest/ch5.pheader.html) by SCO, Inc.
     pub memory_size: u64,
+
+    /// This field indicates the alignment of the segment in the memory.
+    ///
+    /// ## See also
+    /// - [Program Header](https://www.sco.com/developers/gabi/latest/ch5.pheader.html) by SCO, Inc.
     pub alignment: u64
 }
 
 impl ProgramHeader {
+    /// This function reads the data from the section (with offset) and parses it into a
+    /// [ProgramHeader] structure for the ELF file.
+    ///
+    /// Here is a list with all errors, which can occur while this operation:
+    /// - [Error::InvalidClass] - The provided ELF file's class is not valid
+    ///
+    /// ## See also
+    /// - [Program Header](https://www.sco.com/developers/gabi/latest/ch5.pheader.html) by SCO, Inc.
     pub fn read(ident: &ElfIdent, slice: &[u8], mut offset: usize) -> Result<ProgramHeader, Error> {
         let endian = &ident.endian;
         let mut program_header = ProgramHeader::default();
